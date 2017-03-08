e2c03a3d41a6e5b6d75f339d8bda191cfc0a5544
HanYoungsoo <gglife333@gmail.com>
2017-03-08 22:45:08 +0900

    뷰컨트롤러 분업 가져가세요!!~!!

---
+++ HoleIN/HoleIN/PlusViewController.m
@@ -0,0 +1,37 @@
+//
+//  PlusViewController.m
+//  HoleIN
+//
+//  Created by HanYoungsoo on 2017. 3. 8..
+//  Copyright © 2017년 YoungsooHan. All rights reserved.
+//
+
+#import "PlusViewController.h"
+
+@interface PlusViewController ()
+
+@end
+
+@implementation PlusViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ HoleIN/HoleIN/SuccessViewController.h
@@ -0,0 +1,13 @@
+//
+//  SuccessViewController.h
+//  HoleIN
+//
+//  Created by HanYoungsoo on 2017. 3. 8..
+//  Copyright © 2017년 YoungsooHan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SuccessViewController : UIViewController
+
+@end
